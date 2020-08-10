0d8fc137feaea0d2da7f149a6a429d847feee3af
Elon Lima <elon.blima@gmail.com>
2020-08-10 12:43:39 -0300

    proffy

---
+++ src/database/createProffy.js
@@ -0,0 +1,67 @@
+module.exports = async function (db, {proffyValue, classValue, classScheduleValues}) { 
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            "${proffyValue.avatar}",
+            "${proffyValue.name}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"
+        );
+    `)
+
+    const proffy_id = insertedProffy.lastID
+
+    const insertedClass = await db.run(`
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+            );
+    `)
+
+    const class_id = insertedClass.lastID
+
+    const insertedAllClassesScheduleValues = classScheduleValues.map( (classScheduleValue) => {
+        return db.run(`
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+            );
+        `)
+    })
+
+    // await Promise.all(insertedAllClassesScheduleValues)
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+    
+}
---
+++ src/pages.js
@@ -0,0 +1,57 @@
+const database = require('./database/db')
+
+const {subjects, weekdays, getSubject, convertHoursToMinutes} = require('./utils/format')
+
+function pageLanding(req,res) {
+    return res.render("index.html")
+}
+
+async function pageStudy(req,res) {
+    const filters = req.query 
+
+    if (!filters.subject || !filters.weekdays || !filters.time) {
+        return res.render("study.html", { filters, subjects, weekdays })
+    }
+
+    const timeToMinutes = convertHoursToMinutes(filters.time)
+
+    const query = `
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE EXISTS (
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = classes.id
+            AND class_schedule.weekday = ${filters.weekdays}
+            AND class_schedule.time_from <= ${timeToMinutes}
+            AND class_schedule.time_to > ${timeToMinutes}
+        )
+        AND classes.subject = '${filters.subject}'
+    `
+    try {
+        const db = await database
+        const proffys = await db.all(query)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function pageGiveClasses(req,res) {
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+        data.subject = getSubject()
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+    
+    return res.render("give-classes.html", { subjects, weekdays })
+}
+
+module.exports = {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses
+}
